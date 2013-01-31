423c88a8633d5a8c4c2a5a163a6056e392bc7802
luisillosevillano1988@gmail.com <luisillosevillano1988@gmail.com@f96de8dd-ca71-896f-8ff3-121bd0fc4672>
2013-01-31 10:10:30 +0000

    Modificación para añadir campos a la tabla calendario (aun no funcional)

--- trunk/Proyecto tablas y árboles/src/tablas/ControladorCalendario.java
+++ trunk/Proyecto tablas y árboles/src/tablas/ControladorCalendario.java
@@ -4,19 +4,21 @@ import java.util.GregorianCalendar;
 
 public class ControladorCalendario {
 	
-	private VistaCalendario vc;
+	private static VistaCalendario vc;
 	
 	public ControladorCalendario(VistaCalendario vc){
 		this.vc = vc;
 	}
 	
 	public static void añadirDias(int mes, int año){
-		
 		GregorianCalendar gc = new GregorianCalendar(año, mes, 1);
-		gc.setFirstDayOfWeek(GregorianCalendar.MONDAY);
-		System.out.println(gc.get(GregorianCalendar.DAY_OF_WEEK)+" "+año+" "+mes);
-		System.out.println(gc.getFirstDayOfWeek());
-		
+		for(int i = 0; i < gc.getActualMaximum(GregorianCalendar.DAY_OF_MONTH); i++){
+			gc.set(año, mes, i+1);
+			int columna = gc.get(GregorianCalendar.DAY_OF_WEEK) - 2;
+			if(columna < 0)
+				columna = 6;
+			vc.getCalendario().setValueAt(i+1, gc.get(GregorianCalendar.DAY_OF_WEEK_IN_MONTH), columna);
+		}
 	}
 
 }

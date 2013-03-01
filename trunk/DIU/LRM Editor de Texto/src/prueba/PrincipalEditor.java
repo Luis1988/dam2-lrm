a58e3240edcf3646d393d414efac7f36bb6cfe54
luisillosevillano1988@gmail.com <luisillosevillano1988@gmail.com@f96de8dd-ca71-896f-8ff3-121bd0fc4672>
2013-03-01 11:30:59 +0000

    Añadida documentación que faltaba en el main

--- trunk/DIU/LRM Editor de Texto/src/prueba/PrincipalEditor.java
+++ trunk/DIU/LRM Editor de Texto/src/prueba/PrincipalEditor.java
@@ -1,9 +1,14 @@
 package prueba;
 
+/**
+ * Clase principal que hará funcionar el programa
+ * @author Luis Romero Moreno
+ * @version v1.9
+ */
 public class PrincipalEditor {
 
 	/**
-	 * @param args
+	 * @param args String[] que recibe argumentosde la máquina virtual
 	 */
 	public static void main(String[] args) {
 		new PanelEditor();

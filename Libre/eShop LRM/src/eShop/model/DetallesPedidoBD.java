fdb2371bbfca94e1307465746e9349b6f46a979a
luisillosevillano1988@gmail.com <luisillosevillano1988@gmail.com@f96de8dd-ca71-896f-8ff3-121bd0fc4672>
2013-03-04 20:28:32 +0000

    Agregados y eliminados algunos ficheros

---
+++ Libre/eShop LRM/src/eShop/model/DetallesPedidoBD.java
@@ -0,0 +1,14 @@
+package eShop.model;
+
+import java.sql.Statement;
+import java.util.Hashtable;
+
+public class DetallesPedidoBD {
+
+	public static void insertarDetallesPedido(Statement stmt, long idPedido,
+			Hashtable carritoCompra) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
